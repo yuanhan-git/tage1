ab837c9516cdf2cfed6272633948b6888da9113b
yuanhan <15025639215yh.>
2026-03-30 11:57:18 +0800

    也在login下修改login第二行内容以及新增格外的文件1.vue

---
+++ pagess/home/green/1.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        还新增了一个文件
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pagess/login/index.vue
+++ pagess/login/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         这是一个登录页面
+        green也在这里添加一行内容
     </div>
 </template>
 
